c4e927b3bf80b51136976b87dac500d27c773c72
MarinaA2015 <m.abrashkina2015@gmail.com>
2020-07-27 01:36:20 +0300

    Tests launching, testng.xml, groups

--- src/test/java/ru/stqa/selenium/pages/LoginPageHelper.java
+++ src/test/java/ru/stqa/selenium/pages/LoginPageHelper.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPageHelper extends PageBase{
+    
     @FindBy(linkText = "Log In")
     WebElement logInIcon;
 
